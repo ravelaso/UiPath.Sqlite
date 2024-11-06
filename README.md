79299da4859211aa12d20999978499e794610069
Andres Ravelo <ravelaso@outlook.com>
2024-11-06 13:28:16 +0100

    refactoring

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sqlite for UiPath
+
+This set of activities allows you to work with a Sqlite files in UiPath
+
+- Compatibility: net6.0 / UiPath Windows C# Project.
+
+# Issues:
+
+In order to use the SQLite.Interop.dll inside UiPath we need to copy it to our UiPath project root directory.
+You can find these executables within the lib folder at the root of this codebase.
--- Ravelaso.UiPath.Sqlite/ActivityTemplate.cs
+++ Ravelaso.UiPath.Sqlite/ActivityTemplate.cs
@@ -2,70 +2,70 @@
 using System.ComponentModel;
 using System.Data;
 using System.Data.SQLite;
-namespace Ravelaso.UiPath.Sqlite
+
+namespace Ravelaso.UiPath.Sqlite;
+
+[DisplayName("CreateConnection")]
+[Description("Creates a connection object for the SQLite database")]
+public class CreateConnection : CodeActivity
 {
-    
-    [DisplayName("CreateConnection")]
-    [Description("Creates a connection object for the SQLite database")]
-    public class CreateConnection : CodeActivity
-    {
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The name of the SQLite database")]
-        public InArgument<string> DatabasePath { get; set; }
-        
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The database connection object")]
-        public OutArgument<SQLiteConnection> Connection { get; set; }
-        
-        protected override void Execute(CodeActivityContext context)
-        {
-            Connection.Set(context, SqliteHelper.CreateConnection(context.GetValue(DatabasePath)));
-        }
-    }
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The name of the SQLite database")]
+    public InArgument<string> DatabasePath { get; set; }
 
-    [DisplayName("ExecuteQuery")]
-    [Description("Returns a DataTable from a query to the database connection")]
-    public class ExecuteQuery : CodeActivity
-    {
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The SQL Statement")]
-        public InArgument<string> Query { get; set; }
-        
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The database connection object")]
-        public InArgument<SQLiteConnection> Connection { get; set; }
-        
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The result DataTable")]
-        public OutArgument<DataTable> Output { get; set; }
-        protected override void Execute(CodeActivityContext context)
-        {
-            Output.Set(context, SqliteHelper.ExecuteQuery(context.GetValue(Connection), context.GetValue(Query)));
-        }
-    }
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The database connection object")]
+    public OutArgument<SQLiteConnection> Connection { get; set; }
 
-    [DisplayName("ExecuteNonQuery")]
-    [Description("Executes a non query command using a database connection")]
-    public class ExecuteNonQuery : CodeActivity
+    protected override void Execute(CodeActivityContext context)
     {
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The sql non query command")]
-        public InArgument<string> Command { get; set; }
-        
-        [RequiredArgument]
-        [Category("Database")]
-        [Description("The database connection object")]
-        public InArgument<SQLiteConnection> Connection { get; set; }
-        protected override void Execute(CodeActivityContext context)
-        {
-            SqliteHelper.ExecuteNonQuery(context.GetValue(Command), context.GetValue(Connection));
-        }
+        Connection.Set(context, SqliteHelper.CreateConnection(context.GetValue(DatabasePath)));
     }
-    
+}
+
+[DisplayName("ExecuteQuery")]
+[Description("Returns a DataTable from a query to the database connection")]
+public class ExecuteQuery : CodeActivity
+{
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The SQL Statement")]
+    public InArgument<string> Query { get; set; }
+
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The database connection object")]
+    public InArgument<SQLiteConnection> Connection { get; set; }
+
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The result DataTable")]
+    public OutArgument<DataTable> Output { get; set; }
+
+    protected override void Execute(CodeActivityContext context)
+    {
+        Output.Set(context, SqliteHelper.ExecuteQuery(context.GetValue(Connection), context.GetValue(Query)));
+    }
+}
+
+[DisplayName("ExecuteNonQuery")]
+[Description("Executes a non query command using a database connection")]
+public class ExecuteNonQuery : CodeActivity
+{
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The sql non query command")]
+    public InArgument<string> Command { get; set; }
+
+    [RequiredArgument]
+    [Category("Database")]
+    [Description("The database connection object")]
+    public InArgument<SQLiteConnection> Connection { get; set; }
+
+    protected override void Execute(CodeActivityContext context)
+    {
+        SqliteHelper.ExecuteNonQuery(context.GetValue(Command), context.GetValue(Connection));
+    }
 }
