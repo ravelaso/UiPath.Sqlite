71a83138a57bcc58617130d39aa873c8197a0b41
Andres Ravelo <ravelaso@outlook.com>
2024-11-21 08:14:21 +0100

    some unit tests

--- Ravelaso.UiPath.Sqlite.Tests/Unit/ActivityTemplateUnitTests.cs
+++ Ravelaso.UiPath.Sqlite.Tests/Unit/ActivityTemplateUnitTests.cs
@@ -1,13 +1,26 @@
-﻿using Xunit;
+﻿using System.Data.SQLite;
+using Xunit;
 
 namespace Ravelaso.UiPath.Sqlite.Tests.Unit
 {
     public class ActivityTemplateUnitTests
     {
         [Fact]
-        public void Test()
+        public void TestExecuteNonQueryMethod()
         {
-            Assert.Equal(0, 0);
+            // Arrange
+            var databasePath = $@"C:\Data\Dev\testdb.db";
+            var dbSource = $"Data Source={databasePath}";
+            using var conn = new SQLiteConnection(dbSource);
+            var sql = @"DELETE FROM demo_table";
+            try
+            {
+                SqliteHelper.ExecuteNonQuery(sql, conn);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
         }
     }
 }
--- Ravelaso.UiPath.Sqlite.Tests/Workflow/ActivityTemplateWorkflowTests.cs
+++ Ravelaso.UiPath.Sqlite.Tests/Workflow/ActivityTemplateWorkflowTests.cs
@@ -2,33 +2,41 @@
 using System.Data;
 using System.Data.SQLite;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Ravelaso.UiPath.Sqlite.Tests.Workflow
 {
     public class ActivityTemplateWorkflowTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ActivityTemplateWorkflowTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void TestCreateConnection()
         {
             // Arrange
-            var databasePath = @"C:\Data\Dev\database.db";
+            var databasePath = @"C:\Data\Dev\testdb.db";
             var createConnectionActivity = new CreateConnection
             {
                 DatabasePath = new InArgument<string>(databasePath)
             };
-            
+
             var workflowInvoker = new WorkflowInvoker(createConnectionActivity);
-            
+
             // Act & Assert
-            var exception = Record.Exception(() => 
+            var exception = Record.Exception(() =>
             {
                 var outputs = workflowInvoker.Invoke();
                 Assert.NotNull(outputs);
                 Assert.True(outputs.ContainsKey("Connection"));
-                
+
                 var connection = outputs["Connection"] as SQLiteConnection;
                 Assert.NotNull(connection);
-                Assert.Equal(System.Data.ConnectionState.Open, connection.State);
+                Assert.Equal(ConnectionState.Open, connection.State);
 
                 // Cleanup
                 connection.Close();
@@ -38,10 +46,37 @@ namespace Ravelaso.UiPath.Sqlite.Tests.Workflow
         }
 
         [Fact]
+        public void TestExecuteNonQuery()
+        {
+            // Arrange
+            var databasePath = $@"C:\Data\Dev\testdb.db";
+            var dbSource = $"Data Source={databasePath}";
+            using var conn = new SQLiteConnection(dbSource);
+            conn.Open();
+            var sql = @"DELETE FROM demo_table";
+            var executeNonQuery = new ExecuteNonQuery()
+            {
+                Connection = new InArgument<SQLiteConnection>(conn),
+                Command = new InArgument<string>(sql)
+            };
+            var workflowInvoker = new WorkflowInvoker(executeNonQuery);
+                
+            // Act & Assert
+            var exception = Record.Exception(() =>
+            {
+                var outputs = workflowInvoker.Invoke();
+                Assert.NotNull(outputs);
+                    
+            });
+            if (exception != null) _testOutputHelper.WriteLine(exception.Message);
+            conn.Close();
+        }
+
+        [Fact]
         public void TestInsertDataTableCsv()
         {
             // Arrange
-            var databasePath = @"C:\Data\Dev\database.db";
+            var databasePath = @"C:\Data\Dev\testdb.db";
             var excelFilePath = @"C:\Data\Dev\demo_table.csv";
             var tableName = "demo_table";
 
@@ -57,39 +92,40 @@ namespace Ravelaso.UiPath.Sqlite.Tests.Workflow
                 DataTable = new InArgument<DataTable>(dt)
             };
             var workflowInvoker = new WorkflowInvoker(insertDataTableActivity);
-                
+
             var exception = Record.Exception(() =>
             {
                 var outputs = workflowInvoker.Invoke();
                 Assert.NotNull(outputs);
             });
         }
+
         [Fact]
-                public void TestInsertDataTableExcel()
-                {
-                    // Arrange
-                    var databasePath = @"C:\Data\Dev\database.db";
-                    var excelFilePath = @"C:\Data\Dev\demo_table_1k.xlsx";
-                    var tableName = "demo_table";
-        
-                    var dt = Utils.ReadExcelAsDataTable(excelFilePath);
-        
-                    var dbSource = $"Data Source={databasePath}";
-                    using var dbConn = new SQLiteConnection(dbSource);
-                    dbConn.Open();
-                    var insertDataTableActivity = new InsertDataTable()
-                    {
-                        Connection = new InArgument<SQLiteConnection>(dbConn),
-                        TableName = new InArgument<string>(tableName),
-                        DataTable = new InArgument<DataTable>(dt)
-                    };
-                    var workflowInvoker = new WorkflowInvoker(insertDataTableActivity);
-                        
-                    var exception = Record.Exception(() =>
-                    {
-                        var outputs = workflowInvoker.Invoke();
-                        Assert.NotNull(outputs);
-                    });
-                }
+        public void TestInsertDataTableExcel()
+        {
+            // Arrange
+            var databasePath = @"C:\Data\Dev\testdb.db";
+            var excelFilePath = @"C:\Data\Dev\demo_table_1k.xlsx";
+            var tableName = "demo_table";
+
+            var dt = Utils.ReadExcelAsDataTable(excelFilePath);
+
+            var dbSource = $"Data Source={databasePath}";
+            using var dbConn = new SQLiteConnection(dbSource);
+            dbConn.Open();
+            var insertDataTableActivity = new InsertDataTable()
+            {
+                Connection = new InArgument<SQLiteConnection>(dbConn),
+                TableName = new InArgument<string>(tableName),
+                DataTable = new InArgument<DataTable>(dt)
+            };
+            var workflowInvoker = new WorkflowInvoker(insertDataTableActivity);
+
+            var exception = Record.Exception(() =>
+            {
+                var outputs = workflowInvoker.Invoke();
+                Assert.NotNull(outputs);
+            });
+        }
     }
 }
